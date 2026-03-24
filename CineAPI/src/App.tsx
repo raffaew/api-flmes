04748dce7f3ad5c0001fd8ad247467f957c0debb
Rafael Moreira Dias <rafael.moreira@afixcode.com.br>
2026-03-24 16:18:28 -0300

    json

--- CineAPI/src/App.tsx
+++ CineAPI/src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Movies from "./components/Movies/Movies";
+import MovieDetails from "./Modal/MovieDetails";
+import { MovieDetailsProvider } from "./movieDetailsContext/movieDetailsProvider";
+import { SelectProvider } from "./selectContext/SelectProvider";
 function App() {
-  
   return (
-   <div className="app">
-    <h1>Hellow</h1>
-   </div>
-  )
+    <div className="app">
+      <SelectProvider>
+        <MovieDetailsProvider >
+        <Header />
+        <Movies />
+        <div className="MovieDetails"> <MovieDetails /></div>
+        </MovieDetailsProvider>
+      </SelectProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
